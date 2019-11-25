b075941fc1ae03307c1044c8a45d7defe34d8619
hatout01 <hatout1@gmail.com>
2019-11-24 17:16:06 -0800

    added spotify function and Concert this url

--- liri.js
+++ liri.js
@@ -1,10 +1,106 @@
 require('dotenv').config();
-var keys = require("./keys.js")
-var Spotify = require('node-spotify-api');
 
-var spotify = new Spotify(keys.spotify);
+const keys = require("./keys.js")
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify);
+const moment = require("moment");
+moment().format("MM/DD/YYYY");
+const fs = require('fs');
+const arg = process.argv;
+const axios = require("axios")
 
-var moment = require("moment");
-moment().format();
+// console.log(moment)
 
-var fs = require('fs');
+var inquirer = require("inquirer");
+
+// Switch function to display mainMenu with choices
+// const mainMenu = () => {
+//     inquirer
+//         .prompt({
+//             type: "list",
+//             message: "How can I help you today?",
+//             choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
+//             name: "choice"
+//         }).then(res => {
+//             console.log(res.command)
+//         })
+// }
+
+// mainMenu()
+
+
+let commandTwo = "";
+for (let i = 3; i < arg.length; i++) {
+    if (i != arg.length - 1) {
+        commandTwo += arg[i] + "-";
+    } else {
+        commandTwo += arg[i];
+    }
+}
+let commandOne = arg[2]
+
+switch (commandOne) {
+    case ("spotify-this-song"):
+        console.log(commandTwo);
+        spotifyResult();
+        break;
+    case ("concert-this"):
+        console.log(commandTwo);
+        concertThisResult();
+        break;
+    case ("movie-this"):
+        console.log(commandTwo);
+        movieThisResult();
+        break;
+    case ("do-what-it-says"):
+        console.log(commandTwo);
+        doWhatItSaysResult();
+        break;
+
+    default:
+        console.log("Go homme")
+        break;
+}
+
+
+
+// function for Spotify - `spotify-this-song`
+
+function spotifyResult() {
+    spotify
+        .search({ type: 'track', query: commandTwo })
+        .then(function (response) {
+            console.log(response.tracks.items[0].album.artists[0].name); // Artist name
+            console.log(response.tracks.items[0].preview_url); // Song url
+            console.log(response.tracks.items[0].name); // song name
+            console.log(response.tracks.items[0].album.name); // Album name
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+
+// function for Concert - `concert-this`
+
+
+function concertThisResult() {
+    const url = (`"https://rest.bandsintown.com/artists/${commandTwo}/events?app_id=codingbootcamp"`);
+
+    console.log(url)
+
+}
+
+// function for Movies - `movie-this`
+function movieThisResult() {
+
+
+}
+
+
+// function for elseDo - `do-what-it-says`
+
+function doWhatItSaysResult() {
+
+
+}
